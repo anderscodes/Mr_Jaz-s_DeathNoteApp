15cd23aa4d3ab522b9622d9b02748f9b1d7403c3
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-05-31 14:20:25 +0100

    updated tests with new testPass and testFail functions replacing console.log

--- note.js
+++ note.js
@@ -1,10 +1,9 @@
 function Note(){
-  this.notes = ["good byedfsdhfdshjfsghgshsdghsdkghsekugsduhkgs"];
+  this.notes = [];
 };
 
 Note.prototype.add = function(text){
   this.notes.push(text);
-  console.log(this.notes);
 };
 
 Note.prototype.listNotes = function(){
@@ -14,10 +13,6 @@ Note.prototype.listNotes = function(){
     list.push(this.notes[i].slice(0, 19));
   };
   return list;
-};
-
-Note.prototype.delete = function(index){
-  delete this.notes[index];
 };
 
 Note.prototype.grab = function(index){
